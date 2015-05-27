46398cba333a1961d79b3fab1bb61c38903680de
iantsai <iantsai86@gmail.com>
2015-05-27 11:46:48 -0700

    Initial Commit

---
+++ RottenMangoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenMangoes
+//
+//  Created by Ian Tsai on 2015-05-27.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenMangoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMangoes
+//
+//  Created by Ian Tsai on 2015-05-27.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
